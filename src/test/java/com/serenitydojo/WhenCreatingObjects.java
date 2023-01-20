c603d2d2f205693997bfbab89d800bd4d1fd1b86
dennythampi <103203195+dennythampi@users.noreply.github.com>
2023-01-20 12:44:09 +0000

    Exercise 2

--- src/test/java/com/serenitydojo/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -4,4 +4,16 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class WhenCreatingObjects {
+    @Test
+    public void creating_a_dog() {
+        String name = "Fido";
+        String favoriteFood = "Bone";
+        int age = 5;
+
+        Dog fido = new Dog("Fido","Bone", 5);
+        Assert.assertEquals(fido.getName(), "Fido");
+        Assert.assertEquals(fido.getFavoriteToy(), "Bone");
+        Assert.assertEquals(fido.getAge(), 5);
+    }
+
 }
